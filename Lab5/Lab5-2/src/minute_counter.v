674adcfedcaef821a56a1a0738af77c9f2801edb
amo890806 <amo890806@gmail.com>
2022-07-24 14:57:41 +0800

    init commit

---
+++ Lab5/Lab5-2/src/minute_counter.v
@@ -0,0 +1,26 @@
+module minute_counter (
+    input clk,
+    input rst_n,
+    input count_en,
+    input reset_en,
+    input carry,
+    output reg [5:0] binary
+);
+
+always @(posedge clk or negedge rst_n or negedge reset_en) begin
+    if(!rst_n)begin
+        binary <= 0;
+    end
+    else if(!reset_en)begin
+        if(!count_en)begin
+            binary <= 0;
+        end
+    end
+    else begin
+        if(count_en)begin
+            binary <= (carry) ? ((binary == 59) ? 0 : binary+1) : binary;
+        end
+    end
+end
+    
+endmodule
---
+++ Lab5/Lab5-2/src/second_counter.v
@@ -0,0 +1,28 @@
+module second_counter (
+    input clk,
+    input rst_n,
+    input count_en,
+    input reset_en,
+    output carry,
+    output reg [5:0] binary 
+);
+
+always @(posedge clk or negedge rst_n or negedge reset_en) begin
+    if(!rst_n)begin
+        binary <= 0;
+    end
+    else if(!reset_en)begin
+        if(!count_en)begin
+            binary <= 0;
+        end
+    end
+    else begin
+        if(count_en)begin
+            binary <= (binary == 59) ? 0 : binary+1;
+        end
+    end
+end
+
+assign carry =  (binary == 59) ? 1 : 0;
+    
+endmodule
